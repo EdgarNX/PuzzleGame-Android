6dc36322fb348e26fdd5793db5d3d4d3905043ab
EdgarNX <edgarnemeth@gmail.com>
2020-12-21 22:53:31 +0200

    working on online

--- AndroidPuzzleGame/app/src/main/java/com/e/androidpuzzlegame/OnlineActivity.java
+++ AndroidPuzzleGame/app/src/main/java/com/e/androidpuzzlegame/OnlineActivity.java
@@ -2,6 +2,7 @@ package com.e.androidpuzzlegame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class OnlineActivity extends AppCompatActivity {
@@ -10,5 +11,8 @@ public class OnlineActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_online);
+
+        Intent intent = new Intent(this, WaitingRoomActivity.class);
+        startActivity(intent);
     }
 }
