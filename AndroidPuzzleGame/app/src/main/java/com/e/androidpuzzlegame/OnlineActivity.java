00626d5593c732a8fc63df00aa894f9917714d1b
EdgarNX <edgarnemeth@gmail.com>
2021-01-03 21:52:27 +0200

    still working on multiplayer

--- AndroidPuzzleGame/app/src/main/java/com/e/androidpuzzlegame/OnlineActivity.java
+++ AndroidPuzzleGame/app/src/main/java/com/e/androidpuzzlegame/OnlineActivity.java
@@ -33,7 +33,6 @@ public class OnlineActivity extends AppCompatActivity implements View.OnClickLis
     public static final String TABLE_MESSAGE_KEY = "currentTableName";
     public static final String NICKNAME_MESSAGE_KEY = "currentNickname";
 
-
     private String nickname = "";
     private EditText createRoomEditText;
     private EditText joinRoomEditText;
@@ -52,91 +51,26 @@ public class OnlineActivity extends AppCompatActivity implements View.OnClickLis
 
         Button createButton = findViewById(R.id.create_button);
         createRoomEditText = findViewById(R.id.create_room_edit_text);
+
         createButton.setOnClickListener(v -> {
             if (createRoomEditText.getText().toString().matches("^[a-z0-9]*$") && createRoomEditText.getText().toString().length() >= 4) {
                 createRoomEditText.setError(null);
 
-                ParseObject gameTable = new ParseObject("GameTable");
-                gameTable.put("name", createRoomEditText.getText().toString());
-                gameTable.put("host", nickname);
-                gameTable.put("playerTwo", "00");
-                gameTable.put("playerThree", "00");
-                gameTable.put("playerFour", "00");
-                gameTable.put("triggerDestroy", "no");
-                gameTable.put("triggerStop", "no");
-                gameTable.put("triggerStart", "no");
-                gameTable.put("plOneTime", "no");
-                gameTable.put("plTwoTime", "no");
-                gameTable.put("plThreeTime", "no");
-                gameTable.put("plFourTime", "no");
-                gameTable.saveInBackground(new SaveCallback() {
-                    @Override
-                    public void done(ParseException e) {
-                        if (e == null) {
-                            Log.i("SaveInBackgound", "Successful");
-
-                            Intent intent = new Intent(OnlineActivity.this, SelectActivity.class);
-                            intent.putExtra("online", 1);
-                            startActivity(intent);
-
-                            joinRoomEditText.setText("");
-                            createRoomEditText.setText("");
-
-                        } else {
-                            Log.i("SaveInBackgound", "Failed. Error: " + e.toString());
-
-                            joinRoomEditText.setText("");
-                            createRoomEditText.setText("");
-                        }
-                    }
-                });
-
-            } else {
-                createRoomEditText.setError("The room name only contain letters/numbers and must > 3 characters!");
-                joinRoomEditText.setText("");
-                createRoomEditText.setText("");
-            }
-
-        });
-        Button joinButton = findViewById(R.id.join_button);
-        joinRoomEditText = findViewById(R.id.join_room_edit_text);
-        joinButton.setOnClickListener(v -> {
-            if (joinRoomEditText.getText().toString().matches("^[a-z0-9]*$") && joinRoomEditText.getText().toString().length() >= 4) {
-                joinRoomEditText.setError(null);
-
-
-
-                // TODO database verification
-                // todo done
-
                 ParseQuery<ParseObject> query = ParseQuery.getQuery("GameTable");
-                query.whereEqualTo("name", joinRoomEditText.getText().toString());
+                query.whereEqualTo("name", createRoomEditText.getText().toString());
                 query.findInBackground(new FindCallback<ParseObject>() {
                     @Override
                     public void done(List<ParseObject> objects, ParseException e) {
-                        if (e == null && !objects.isEmpty()) {
-
-                            if ("true".equals(objects.get(0).get("available").toString())) {
-
-                                Intent intent = new Intent(OnlineActivity.this, WaitingRoomActivity.class);
-                                intent.putExtra(TABLE_MESSAGE_KEY, joinRoomEditText.getText().toString());
-                                intent.putExtra(NICKNAME_MESSAGE_KEY, nickname);
-                                startActivity(intent);
-
-                                joinRoomEditText.setText("");
-                                createRoomEditText.setText("");
-
-                            } else {
-                                toast = Toast.makeText(getApplicationContext(), "The room is not available!", Toast.LENGTH_SHORT);
+                        if (e == null) {
+                            if (!objects.isEmpty()) {
+                                toast = Toast.makeText(getApplicationContext(), "The room already exists!", Toast.LENGTH_SHORT);
                                 toast.setGravity(Gravity.CENTER, 0, 0);
                                 toast.show();
-
-                                joinRoomEditText.setText("");
-                                createRoomEditText.setText("");
+                            } else {
+                                createRoom();
                             }
-
                         } else {
-                            toast = Toast.makeText(getApplicationContext(), "Something went wrong!", Toast.LENGTH_SHORT);
+                            toast = Toast.makeText(getApplicationContext(), "Something went wrong11111!", Toast.LENGTH_SHORT);
                             toast.setGravity(Gravity.CENTER, 0, 0);
                             toast.show();
 
@@ -146,16 +80,99 @@ public class OnlineActivity extends AppCompatActivity implements View.OnClickLis
                     }
                 });
             } else {
+                createRoomEditText.setError("The room name only contain letters/numbers and must > 3 characters!");
+                joinRoomEditText.setText("");
+                createRoomEditText.setText("");
+            }
+        });
+
+        Button joinButton = findViewById(R.id.join_button);
+        joinRoomEditText = findViewById(R.id.join_room_edit_text);
+
+        joinButton.setOnClickListener(v -> {
+            if (joinRoomEditText.getText().toString().matches("^[a-z0-9]*$") && joinRoomEditText.getText().toString().length() >= 4) {
+                joinRoomEditText.setError(null);
+                joinRoom();
+            } else {
                 joinRoomEditText.setError("The room name only contain letters/numbers and must > 3 characters!");
             }
+        });
+    }
 
+    public void joinRoom() {
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("GameTable");
+        query.whereEqualTo("name", joinRoomEditText.getText().toString());
+        query.findInBackground(new FindCallback<ParseObject>() {
+            @Override
+            public void done(List<ParseObject> objects, ParseException e) {
+                if (e == null && !objects.isEmpty()) {
+                    if ("true".equals(objects.get(0).get("available").toString())) {
+                        Intent intent = new Intent(OnlineActivity.this, WaitingRoomActivity.class);
+                        intent.putExtra(TABLE_MESSAGE_KEY, joinRoomEditText.getText().toString());
+                        intent.putExtra(NICKNAME_MESSAGE_KEY, nickname);
+                        startActivity(intent);
+
+                        joinRoomEditText.setText("");
+                        createRoomEditText.setText("");
+                    } else {
+                        toast = Toast.makeText(getApplicationContext(), "The room is not available!", Toast.LENGTH_SHORT);
+                        toast.setGravity(Gravity.CENTER, 0, 0);
+                        toast.show();
+
+                        joinRoomEditText.setText("");
+                        createRoomEditText.setText("");
+                    }
+                } else {
+                    toast = Toast.makeText(getApplicationContext(), "Something went wrong!", Toast.LENGTH_SHORT);
+                    toast.setGravity(Gravity.CENTER, 0, 0);
+                    toast.show();
+
+                    joinRoomEditText.setText("");
+                    createRoomEditText.setText("");
+                }
+            }
+        });
+    }
+
+    public void createRoom() {
+        ParseObject gameTable = new ParseObject("GameTable");
+        gameTable.put("name", createRoomEditText.getText().toString());
+        gameTable.put("host", nickname);
+        gameTable.put("playerTwo", "00");
+        gameTable.put("playerThree", "00");
+        gameTable.put("playerFour", "00");
+        gameTable.put("triggerDestroy", "no");
+        gameTable.put("triggerStop", "no");
+        gameTable.put("triggerStart", "no");
+        gameTable.put("plOneTime", "no");
+        gameTable.put("plTwoTime", "no");
+        gameTable.put("plThreeTime", "no");
+        gameTable.put("plFourTime", "no");
+        gameTable.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e == null) {
+                    Log.i("SaveInBackgound", "Successful");
+
+                    Intent intent = new Intent(OnlineActivity.this, SelectActivity.class);
+                    intent.putExtra("online", 1);
+                    startActivity(intent);
+
+                    joinRoomEditText.setText("");
+                    createRoomEditText.setText("");
+                } else {
+                    Log.i("SaveInBackgound", "Failed. Error: " + e.toString());
+
+                    joinRoomEditText.setText("");
+                    createRoomEditText.setText("");
+                }
+            }
         });
     }
 
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.nickname_menu, menu);
-
         return true;
     }
 
